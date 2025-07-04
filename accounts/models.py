11f25ee2cc878da47c621de3f9308ba62829f288
shooter-dev <vincentbleach@gmail.com>
2025-07-04 12:20:47 +0200

    fix: update CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     is_coach = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
