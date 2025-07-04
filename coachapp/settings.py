39c1dbdae67c70f06df714dc378e3039e046d481
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2025-07-04 13:02:24 +0200

    Accounts (#2)
    * feat: create app accounts
    
    * feat: add app accounts to project
    
    * feat: create CustomUser
    
    * fix: update CustomUser
    
    * fix: add gestion CustomUserAdmin admin
    
    * fix: add form login
    
    * fix: add form CustomUserCreationForm
    
    * fix: add template login && base && static file css && update config
    
    * fix: add view signup_view
    
    * fix: add urls login, signup, logout

--- coachapp/settings.py
+++ coachapp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'coachapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+LOGOUT_REDIRECT_URL = 'login'
