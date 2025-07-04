e6c5951a5bebc33b83fe66ba91813efee2f5306d
shooter-dev <vincentbleach@gmail.com>
2025-07-04 12:17:20 +0200

    feat: create CustomUser

--- coachapp/settings.py
+++ coachapp/settings.py
@@ -120,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+LOGOUT_REDIRECT_URL = 'login'
