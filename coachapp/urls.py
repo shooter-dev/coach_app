5bfde623859c7c665a96b342c62b24cead8d4945
shooter-dev <vincentbleach@gmail.com>
2025-07-09 12:22:10 +0200

    feat : creation de la page info

--- coachapp/urls.py
+++ coachapp/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from coachapp.views import info_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('info/', info_view, name='info'),
     path('dashboard/', include('dashboard.urls')),
 ]
