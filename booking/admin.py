ee0eef4366c3c46cbf5fb9e31a0e61293aea0b81
shooter-dev <vincentbleach@gmail.com>
2025-07-09 09:31:37 +0200

    create model booking, admin booking, migration booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from booking.models import Booking
+
+
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+    list_display = ['date', 'heure_debut', 'client', 'objet', 'note_coach']
+
+admin.site.register(Booking, BookingAdmin)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from coachapp import settings
+
+
+class Booking(models.Model):  # ou Booking
+    date = models.DateField()
+    heure_debut = models.TimeField()
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    objet = models.CharField(max_length=255)
+    note_coach = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('date', 'heure_debut')  # pas de double réservation
+
+    def __str__(self):
+        return f"{self.date} {self.heure_debut} – {self.client.username}"
+
